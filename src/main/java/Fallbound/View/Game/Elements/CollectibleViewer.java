4a63f336bfaa4695866fa03891b8b8ef1d32ef53
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2024-12-15 21:46:18 +0000

    formatting

--- src/main/java/Fallbound/View/Game/Elements/CollectibleViewer.java
+++ src/main/java/Fallbound/View/Game/Elements/CollectibleViewer.java
@@ -6,7 +6,7 @@ import Fallbound.Model.Vector;
 import Fallbound.View.Theme;
 
 public class CollectibleViewer implements ElementViewer<Collectible> {
-    
+
     @Override
     public void draw(GUI gui, Collectible element, int offset) {
         gui.drawText(element.getPosition().toPosition().applyOffset(offset), element.getIcon(), Theme.FALLBOUND_GOLD);
