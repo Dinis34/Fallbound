b791a3bfc3dee81016f3ade8481a89d69452c69a
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2024-12-17 02:39:01 +0000

    trail

--- src/main/java/Fallbound/View/Theme.java
+++ src/main/java/Fallbound/View/Theme.java
@@ -2,7 +2,7 @@ package Fallbound.View;
 
 public class Theme {
     public static final String FALLBOUND_RED = "#E33131";
-    public static final String FALLBOUND_BLUE = "#0040BF";
+    public static final String FALLBOUND_BLUE = "#3377ff";
     public static final String FALLBOUND_GOLD = "#D8A500";
     public static final String FALLBOUND_WHITE = "#FFFFFF";
     public static final String FALLBOUND_LIGHT_GRAY = "#555555";
