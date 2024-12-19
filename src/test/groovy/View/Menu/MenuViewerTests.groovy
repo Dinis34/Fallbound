214eda92d6005325650bcb3f3a3f1828b7604b67
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2024-12-19 17:54:40 +0000

    fixed get option test

--- src/test/groovy/View/Menu/MenuViewerTests.groovy
+++ src/test/groovy/View/Menu/MenuViewerTests.groovy
@@ -5,6 +5,7 @@ import Fallbound.Model.Menu.GameOverMenu
 import Fallbound.Model.Menu.PauseMenu
 import Fallbound.Model.Menu.StartMenu
 import Fallbound.Model.Position
+import Fallbound.View.Theme
 import Fallbound.View.Menu.GameOverMenuViewer
 import Fallbound.View.Menu.PauseMenuViewer
 import Fallbound.View.Menu.StartMenuViewer
@@ -53,23 +54,111 @@ class MenuViewerTests extends Specification {
 
     }
 
-    def "Draw Options"() {
+    def "Draw Options correctly renders all menu items with second option selected"() {
         given:
-        def MenuViewer = Spy(PauseMenuViewer.class)
         def gui = Mock(GUI)
         def pauseMenu = new PauseMenu()
+        def menuViewer = Spy(PauseMenuViewer, constructorArgs: [pauseMenu]) {
+            getModel() >> pauseMenu
+        }
 
-        pauseMenu.nextOption()
-        MenuViewer.getModel() >> pauseMenu
+        // Select "restart" option
+        pauseMenu.nextOption()  // Moves to second option (restart)
 
         when:
-        MenuViewer.drawOptions(gui)
+        menuViewer.drawOptions(gui)
 
         then:
-        1 * MenuViewer.drawOptions(gui)
-        1 * gui.drawText(new Position(10, 5 + 1 * 1), "\u2592" + pauseMenu.getOption(1), _)
-        1 * gui.drawText(new Position(10, 5 + 1 * 3), pauseMenu.getOption(3), _)
-        2 * gui.drawText(_, _, _)
+        interaction {
+            // "continue" option
+            1 * gui.drawText(
+                    new Position(4, 25),
+                    "continue",
+                    Theme.FALLBOUND_LIGHT_GRAY
+            )
+
+            // "restart" option (selected)
+            1 * gui.drawText(
+                    new Position(4, 26),
+                    "▒ restart",
+                    Theme.FALLBOUND_WHITE
+            )
+
+            // "exit to main menu" option
+            1 * gui.drawText(
+                    new Position(4, 27),
+                    "exit to main menu",
+                    Theme.FALLBOUND_LIGHT_GRAY
+            )
+
+            // "exit to desktop" option
+            1 * gui.drawText(
+                    new Position(4, 28),
+                    "exit to desktop",
+                    Theme.FALLBOUND_LIGHT_GRAY
+            )
+
+            // Verify no other calls to drawText
+            0 * gui.drawText(_, _, _)
+        }
+
+        and: "verify correct option is selected"
+        pauseMenu.isSelectedRestart()
+        !pauseMenu.isSelectedContinue()
+        !pauseMenu.isSelectedExit()
+        !pauseMenu.isSelectedExitToDesktop()
+    }
+
+    def "Draw Options correctly renders with last option selected"() {
+        given:
+        def gui = Mock(GUI)
+        def pauseMenu = new PauseMenu()
+        def menuViewer = Spy(PauseMenuViewer, constructorArgs: [pauseMenu]) {
+            getModel() >> pauseMenu
+        }
+
+        // Select last option (exit to desktop)
+        pauseMenu.nextOption() // to restart
+        pauseMenu.nextOption() // to exit to main menu
+        pauseMenu.nextOption() // to exit to desktop
+
+        when:
+        menuViewer.drawOptions(gui)
+
+        then:
+        interaction {
+            1 * gui.drawText(
+                    new Position(4, 25),
+                    "continue",
+                    Theme.FALLBOUND_LIGHT_GRAY
+            )
+
+            1 * gui.drawText(
+                    new Position(4, 26),
+                    "restart",
+                    Theme.FALLBOUND_LIGHT_GRAY
+            )
+
+            1 * gui.drawText(
+                    new Position(4, 27),
+                    "exit to main menu",
+                    Theme.FALLBOUND_LIGHT_GRAY
+            )
+
+            1 * gui.drawText(
+                    new Position(4, 28),
+                    "▒ exit to desktop",
+                    Theme.FALLBOUND_WHITE
+            )
+
+            0 * gui.drawText(_, _, _)
+        }
+
+        and: "verify correct option is selected"
+        pauseMenu.isSelectedExitToDesktop()
+        !pauseMenu.isSelectedContinue()
+        !pauseMenu.isSelectedRestart()
+        !pauseMenu.isSelectedExit()
     }
 
     def "Draw Menu Title"(){
