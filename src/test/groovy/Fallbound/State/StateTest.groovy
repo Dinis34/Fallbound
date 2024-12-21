7cd79f61e193d33a9cfaa78a3fcbeb42dba8136b
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2024-12-21 19:49:15 +0000

    fixed StateTest

--- src/test/groovy/Fallbound/State/StateTest.groovy
+++ src/test/groovy/Fallbound/State/StateTest.groovy
@@ -103,22 +103,22 @@ class StateTest extends Specification {
         def game = Mock(Game)
         def state = State.getInstance()
         def viewer = Mock(Viewer)
-        def controller = null
+        def controller = Mock(Controller)
         def stateSpy = Spy(state)
 
         stateSpy.setController(controller)
         stateSpy.setViewer(viewer)
 
-        gui.getNextAction() >> null
+        gui.getNextAction() >> new HashSet<>()
+        gui.getNextSingleAction() >> new HashSet<>()
 
         when:
         stateSpy.step(gui, game, 0)
 
         then:
-        1 * stateSpy.StateActions()
         1 * gui.getNextAction()
-        1 * viewer.draw(_, _)
-        0 * controller.step(_, _, _)
+        1 * controller.step(game, _, 0)
+        1 * viewer.draw(gui, 0)
     }
 
     def "State Step when Controller is MenuController"() {
